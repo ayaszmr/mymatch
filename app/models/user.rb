1eb71bba22779dbe7d00647333d90113906a751c
@higakikeita <higaki.keichan@gmail.com>
2020-03-09 21:34:41 +0900

    Revert "enum性別定義"
    This reverts commit d717910c7b845c9908947d43fb2d49cff693ea40.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   mount_uploader :img_name, ImgNameUploader
-  enum sex: { 男: 0, 女: 1 }
 end
