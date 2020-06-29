896485702ecb93438b7b46fa8e9af7da3f21c180
ayaszmr <s.aya@live.com>
2020-06-29 17:05:59 +0900

    showアクション追加

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -14,4 +14,12 @@ class ChatController < ApplicationController
     # チャットルームへ遷移させる
     redirect_to action: :show, id: chat_room.id
   end
+
+  def show
+    # チャット相手の情報を取得する
+    chat_room = ChatRoom.find_by(id: params[:id])
+    @chat_room_user = chat_room.chat_room_users.
+      where.not(user_id: current_user.id).first.user
+    @chat_messages = ChatMessage.where(chat_room: chat_room).order(:created_at)
+  end
 end
